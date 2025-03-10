e51bd78977d1f1d1f488d6fde2de6d2aa4123efe
DaviMBDev <davimb00789@gmail.com>
2025-03-10 10:03:34 -0300

    Condicionais adicionados na 1, 2 e 3

--- entrega1/at2/at2.js
+++ entrega1/at2/at2.js
@@ -1,18 +1,23 @@
-let idade = prompt("Digite sua idade: ")
+function classIdade(){    
+    let idade = prompt("Digite sua idade: ")
 
-function etaria(idade){
-    switch(true){
-        case idade <= 0:
-            return "Erro: Valor inválido" 
-        case idade < 16:
-            return "Criança"
-        case idade < 31:
-            return "Jovem"
-        case idade < 60:
-            return "Adulto"
-        case idade > 59:
-            return "Idoso"
+    function etaria(idade){
+        switch(true){
+            case idade <= 0:
+                return "Erro: Valor inválido" 
+            case (isNaN(idade)):
+                return "Erro: Valor inválido"
+            case idade < 16:
+                return "Criança"
+            case idade < 31:
+                return "Jovem"
+            case idade < 60:
+                return "Adulto"
+            case idade > 59:
+                return "Idoso"
+        }
     }
-}
 
-alert(etaria(idade))
+    alert(etaria(idade))
+}
+classIdade()
--- entrega1/at3/at3.js
+++ entrega1/at3/at3.js
@@ -1,24 +1,40 @@
-let nota1 = prompt("Digite sua nota na Atividade prática de laboratório: ")
-let nota2 = prompt("Digite sua nota na Prova de Semestre: ")
-let nota3 = prompt("Digite sua nota no Trabalho teórico: ")
+function lerNota(){
+    let nota1 = prompt("Digite sua nota na Atividade prática de laboratório: ")
+    let nota2 = prompt("Digite sua nota na Prova de Semestre: ")
+    let nota3 = prompt("Digite sua nota no Trabalho teórico: ")
 
-let media = 0.2*nota1 + 0.5*nota2 + 0.3*nota3
-
-function rank(media){
-    switch(true){
-        case media <= 5:
-            return "F"
-        case media <= 6:
-            return "E"
-        case media <=7:
-            return "D"
-        case media <=8:
-            return "C"
-        case media <=9:
-            return "B"
-        case media > 9:
-            return "A"
+    if (isNaN(nota1) || nota1 < 0){
+        alert("Valor inserido inválido")
+        return
     }
-}
+    if (isNaN(nota2) || nota2 < 0){
+        alert("Valor inserido inválido")
+        return
+    }
+    if (isNaN(nota3) || nota3 < 0){
+        alert("Valor inserido inválido")
+        return
+    }
 
-alert("A média do aluno é = "+media+ " e a sua classificação é "+rank(media))
+    let media = 0.2*nota1 + 0.5*nota2 + 0.3*nota3
+
+    function rank(media){
+        switch(true){
+            case media <= 5:
+                return "F"
+            case media <= 6:
+                return "E"
+            case media <=7:
+                return "D"
+            case media <=8:
+                return "C"
+            case media <=9:
+                return "B"
+            case media > 9:
+                return "A"
+        }
+    }
+
+    alert("A média do aluno é = "+media+ " e a sua classificação é "+rank(media))
+}
+lerNota()
